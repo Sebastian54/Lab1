865cd30b92fd777093e03bbeec337eff1378dfc8
Sebastian <45011245+Sebastian54@users.noreply.github.com>
2025-11-11 15:04:55 +0000

    Lab 1 Examples complete

---
+++ greeting.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hi Seb!");
+    return 0; 
+}
---
+++ table.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Surname     ForeName    Language Designed\n");
+    printf("********    ********    *****************\n");
+    printf("Gosling     Ray         Java\n");
+    printf("Stroustrup  Bjarne      C++\n");
+    printf("Ritchie     Dennis      C\n");
+    printf("Wirth       Niklaus     Pascal");
+    return 0;
+}
