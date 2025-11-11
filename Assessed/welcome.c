55775964790f76e949dbc83bb860ce2182f5a0be
Sebastian <45011245+Sebastian54@users.noreply.github.com>
2025-11-11 15:15:47 +0000

    finished assessment

---
+++ Assessed/welcome.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("******************************************\n");
+    printf("*               Welcome to C             *\n");
+    printf("* A General Purpose Programming Language *\n");
+    printf("*       Developed by Dennis Ritchie      *\n");
+    printf("******************************************\n");
+    return 0;
+}
---
+++ Examples/message.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("This is a string!");
+    return 0; 
+}
